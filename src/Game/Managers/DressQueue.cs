05ee86379fc04ae678e40c7bb769e506a1eb11e4
Storm Kiernan <targetstorm@gmail.com>
2020-10-05 16:43:27 -0700

    Shortening the dress queue delay so that dressing feels more responsive.

--- src/Game/Managers/DressQueue.cs
+++ src/Game/Managers/DressQueue.cs
@@ -6,19 +6,20 @@ namespace ClassicUO.Game.Managers
 {
     internal class DressQueue : IUpdateable
     {
+        const long NETWORK_OBJECT_USAGE_INTERVAL = 600;
         private readonly Deque<Action> _actions = new Deque<Action>();
         private long _timer;
 
         public DressQueue()
         {
-            _timer = Time.Ticks + 1000;
+            _timer = Time.Ticks + NETWORK_OBJECT_USAGE_INTERVAL;
         }
 
         public void Update(double totalMS, double frameMS)
         {
             if (_timer < Time.Ticks)
             {
-                _timer = Time.Ticks + 1000;
+                _timer = Time.Ticks + NETWORK_OBJECT_USAGE_INTERVAL;
 
                 if (_actions.Count == 0)
                 {
